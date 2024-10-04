60ee3187f24fc9d2bc583d44a2624b9220adcaf0
Micah Fitzgerald <mcfitz2@gmail.com>
2024-10-04 16:13:26 +0000

    fix restore flag type

--- plugins/modules/proxmox_backup_restore.py
+++ plugins/modules/proxmox_backup_restore.py
@@ -186,9 +186,9 @@ def main():
             except ResourceException as e:
                 module.fail_json(msg=f"Unable to determine resource type: {str(e)}")
         if resource_type == 'lxc':
-            upid = proxmox.nodes(node).lxc.post(vmid=vmid, ostemplate=backup, node=node, restore=True)
+            upid = proxmox.nodes(node).lxc.post(vmid=vmid, ostemplate=backup, node=node, restore="true")
         elif resource_type == 'qemu':
-            upid = proxmox.nodes(node).qemu.post(vmid=vmid, ostemplate=backup, node=node, restore=True)
+            upid = proxmox.nodes(node).qemu.post(vmid=vmid, ostemplate=backup, node=node, restore="true")
         module.exit_json(changed=True, task_id=upid)
 
     except ResourceException as e:
