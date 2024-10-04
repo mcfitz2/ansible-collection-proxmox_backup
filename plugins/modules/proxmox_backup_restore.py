6e16195a4b7dbda3e5d6eb206f53d5bcdf0130d1
Micah Fitzgerald <mcfitz2@gmail.com>
2024-10-04 15:20:14 +0000

    first prototype of restore module

--- plugins/modules/proxmox_backup_restore.py
+++ plugins/modules/proxmox_backup_restore.py
@@ -49,14 +49,51 @@ options:
         description: Proxmox node that you wish to query
         required: true
         type: str
-    storage:
-        description: Data store that holds the desired backup
-        required: true
-        type: str
     vmid:
         description: VMID of VM/LXC to restore
         type: int
         required: true
+    backup:
+        description: Backup to restore
+        required: true
+        type: strf
+    bandwidth_limit:
+        description: Override I/O bandwidth limit (in KiB/s).
+        type: int
+        required: false
+    storage:
+        description: Destination datastore. If not specified, resource will be restored to its original datastore
+        required: false
+        type: str
+    unique:
+        description: Assign a unique random ethernet address.
+        type: bool
+        required: false
+        default: false
+    start_after_restore:
+        description: Start VM/LXC after restore
+        required: false
+        default: false
+        type: bool
+    override:
+        elements: dict
+        suboptions:
+            unprivileged:
+                description: Makes the container run as unprivileged user. Defaults to value in backup
+                type: bool
+                required: false
+            hostname:
+                description: Override the VM/LXC hostname. Defaults to value in backup
+                type: str
+                required: false
+            memory:
+                description: Override amount of memory (in MB) assigned to the VM/LXC. Defaults to value in backup
+                type: int
+                required: false
+            cores:
+                description: Override number of cores assigned to the VM/LXC. Defaults to value in backup
+                type: int
+                required: false
 
 requirements: [ "proxmoxer" ]
 
@@ -66,55 +103,11 @@ author:
 
 EXAMPLES = r'''
 ---
-# Create a auto tag
-- name: Create a auto tag
-  devopsarr.sonarr.sonarr_auto_tag:
-    remove_tags_automatically: false
-    name: "Type"
-    tags: [1]
-    specifications:
-    - name: "anime"
-      implementation: "SeriesTypeSpecification"
-      negate: false
-      required: true
-      fields:
-      - name: "value"
-        value: 2
-
-# Delete a auto tag
-- name: Delete a auto tag
-  devopsarr.sonarr.sonarr_auto_tag:
-    name: Example
-    state: absent
 '''
 
 RETURN = r'''
 # These are examples of possible return values, and in general should use other names for return values.
-id:
-    description: auto tagID.
-    type: int
-    returned: always
-    sample: 1
-name:
-    description: Name.
-    returned: always
-    type: str
-    sample: "Example"
-remove_tags_automatically:
-    description: Remove tags automatically flag.
-    returned: always
-    type: bool
-    sample: false
-specifications:
-    description: specification list.
-    type: list
-    returned: always
-tags:
-    description: Tag list.
-    type: list
-    returned: always
-    elements: int
-    sample: [1,2]
+
 '''
 
 from ansible.module_utils.basic import missing_required_lib  # noqa: E402
@@ -122,8 +115,8 @@ import traceback  # noqa: E402
 from ansible.module_utils.basic import AnsibleModule  # noqa: E402
 PROXMOXER_IMP_ERR = None
 try:
-    # from proxmoxer import ProxmoxAPI
-    # from proxmoxer import ResourceException
+    from proxmoxer import ProxmoxAPI
+    from proxmoxer import ResourceException
     HAS_PROXMOXER = True
 except ImportError:
     HAS_PROXMOXER = False
@@ -138,9 +131,19 @@ def main():
             api_password=dict(type='str', required=True, no_log=True),
             api_port=dict(type='int', required=False, default=8006),
             node=dict(type='str', required=True),
-            storage=dict(type='str', required=True),
+            backup=dict(type='str', required=True),
             verify_ssl=dict(type='bool', default=True),
-            vmid=dict(type='int', required=True)
+            vmid=dict(type='int', required=True),
+            bandwidth_limit=dict(type='int', default=None),
+            storage=dict(type='str', default=None),
+            unique=dict(type='bool', default=False),
+            start_after_restore=dict(type='bool', default=False),
+            override=dict(
+                privileged=dict(type='str', default=None),
+                hostname=dict(type='str', default=None),
+                memory=dict(type='int', default=None),
+                cores=dict(type='int', default=None)
+            )
         ),
         supports_check_mode=True
     )
@@ -156,19 +159,33 @@ def main():
 
     if module.check_mode:
         module.exit_json(**result)
-    # storage = module.params['storage']
-    # node = module.params['node']
-    # vmid = module.params['vmid']
-    # proxmox = ProxmoxAPI(module.params['api_host'],
-    #                      user=module.params['api_user'],
-    #                      password=module.params['api_password'],
-    #                      verify_ssl=module.params['verify_ssl'])
+    backup = module.params['backup']
+    node = module.params['node']
+    vmid = module.params['vmid']
+    proxmox = ProxmoxAPI(module.params['api_host'],
+                         user=module.params['api_user'],
+                         password=module.params['api_password'],
+                         verify_ssl=module.params['verify_ssl'])
 
-    # try:
-    #     module.exit_json(changed=False)
+    try:
+        resource_type = None
+        try:
+            proxmox.nodes(node).lxc(vmid).get()
+            resource_type = 'lxc'
+        except ResourceException:
+            try:
+                proxmox.nodes(node).lxc(vmid).get()
+                resource_type = 'qemu'
+            except ResourceException as e:
+                module.fail_json(msg=f"Unable to determine resource type: {str(e)}")
+        if resource_type == 'lxc':
+            proxmox.nodes(node).lxc.post(vmid=vmid, ostemplate=backup, node=node)
+        elif resource_type == 'qemu':
+            proxmox.nodes(node).qemu.post(vmid=vmid, ostemplate=backup, node=node)
+        module.exit_json(changed=False)
 
-    # except ResourceException as e:
-    #     module.fail_json(msg=f"A Proxmox error occurred: {str(e)}")
+    except ResourceException as e:
+        module.fail_json(msg=f"A Proxmox error occurred: {str(e)}")
     module.exit_json(changed=False)
 
 
